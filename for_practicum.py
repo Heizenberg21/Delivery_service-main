cd7777fcf3c0b359fef0b5aff7172a6242aa8183
Владимир Князев <knezev2910@bk.ru>
2025-09-26 21:31:27 +0300

    CRYPT

--- for_practicum.py
+++ for_practicum.py
@@ -1,23 +1,28 @@
+import string
+
+ALL_DIGITS = set(string.digits)
+
+
 def encrypted_instructions(encrypted_string: str) -> str:
     '''Функция шифрованные инструкции.'''
-    # посылка 143142428
-    stack = []
-    current_num = ''
-    current_str = ''
-    for el in encrypted_string:
-        if el.isdigit():
-            current_num += el
-        elif el == '[':
-            stack.append((current_str, int(current_num) if current_num else 1))
+    # посылка 143213005
+    stack: list[tuple] = []
+    current_num: str = ''
+    current_str: str = ''
+    for char in encrypted_string:
+        if char in ALL_DIGITS:
+            current_num += char
+        elif char == '[':
+            stack.append((current_str, int(current_num)))
             current_str = ''
             current_num = ''
-        elif el == ']':
+        elif char == ']':
             prev_str, multiplier = stack.pop()
             current_str = prev_str + current_str * multiplier
         else:
-            current_str += el        
+            current_str += char
     return current_str
 
-#if __name__ == 'main':
-sipher_str = input()
-print(encrypted_instructions(sipher_str))
+
+if __name__ == 'main':
+    print(encrypted_instructions(input()))
